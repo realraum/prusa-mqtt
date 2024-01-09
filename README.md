3295ab266a10169b3e400da4331ce09b63e5de41
Maciej Krüger <mkg20001@gmail.com>
2024-01-09 21:59:38 +0100

    feat: make it work

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ NodeJS Daemon to fetch info about prusa printer and publish it to mqtt
 
 Supports multiple printers
 
+# Message
+
+Format:
+
+```
+ {“Printer”:string, “Job”:string, “Elapsed_time_s”:int64, “Progress_percent”:int} 
+```
--- bin.js
+++ bin.js
@@ -1,4 +1,8 @@
+#!/usr/bin/env node
+
 import fs from 'fs'
+import { connect } from 'mqtt'
+import yaml from 'js-yaml'
 
 function PrusaPrinter(host, apiKey) {
 	async function req(url, method = 'GET', headers = {}) {
@@ -28,10 +32,85 @@ function PrusaPrinter(host, apiKey) {
 let config
 
 try {
-  config = yaml.load(fs.readFileSync(process.argv(1), 'utf8'))
+	config = yaml.load(fs.readFileSync(process.argv.pop(), 'utf8'))
 } catch (e) {
-  throw e
+	throw e
 }
 
-import { connect } from 'mqtt'
 const client = connect(config.mqtt)
+
+function Daemon(client, { display, ip, key, topic }) {
+	const printer = PrusaPrinter("http://" + ip, key)
+
+	let lock
+
+	async function wrapper() {
+		try {
+			await lock
+		} catch (e) {
+			// noop
+		}
+		lock = main()
+	}
+
+	async function main() {
+		let info
+
+		try {
+			info = await printer.info()
+		} catch (e) {
+			console.error('Failed to fetch info for %s: %o', display, String(e))
+			await client.publishAsync(topic, Buffer.from([]))
+			return
+		}
+
+		if (info.state === 'Printing' || info.state === 'Finished') {
+			await client.publishAsync(topic, JSON.stringify({
+				Printer: display,
+				Job: info.job.file.display,
+				Elapsed_time_s: info.progress.printTime,
+				Progress_percent: Math.floor(info.progress.completion * 100)
+			}))
+		} else {
+			await client.publishAsync(topic, Buffer.from([]))
+		}
+	}
+
+	let intv
+
+	return {
+		start() {
+			if (!intv) intv = setInterval(main, 1000)
+		},
+		async stop() {
+			clearInterval(intv)
+			await lock
+			await client.publishAsync(topic, Buffer.from([]))
+		}
+	}
+}
+
+let daemons = []
+
+for (const printer of config.printers) {
+	daemons.push(Daemon(client, printer))
+}
+
+for (const daemon of daemons) {
+	daemon.start()
+}
+
+async function stop() {
+	console.log('Stopping...')
+
+	for (const daemon of daemons) {
+		await daemon.stop()
+	}
+
+	await client.endAsync()
+
+	console.log('Stopped!')
+}
+
+process.on('SIGINT', stop)
+process.on('SIGTERM', stop)
