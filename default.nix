07afe5b03a0a0ab678af84bd275f57a59c38b250
Maciej Krüger <mkg20001@gmail.com>
2024-01-11 16:08:05 +0100

    fix: bin

--- default.nix
+++ default.nix
@@ -12,16 +12,14 @@ mkPnpmPackage rec {
 
   distDir = ".gitignore";
 
-  nativeBuildInputs = [
-    makeWrapper
-  ];
+  dontBuild = true;
 
   postInstall = ''
+    patchShebangs bin.js
     rm -rf $out
     cp -r $PWD $out
     ls $out
-    makeWrapper ${nodejs.pkgs.pnpm} $out/bin/prusa-mqtt \
-      --cwd $out \
-      --arg "start"
+    mkdir -p $out/bin
+    ln -s $out/bin.js $out/bin/prusa-mqtt
   '';
 }
