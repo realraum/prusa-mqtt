b12f26ef19253c9b7150d0795144361cc1c2c0f2
Maciej Krüger <mkg20001@gmail.com>
2024-01-11 16:10:17 +0100

    feat: config in nix module

--- module.nix
+++ module.nix
@@ -2,15 +2,26 @@
 
 with lib;
 
+let
+  format = pkgs.formats.yaml {};
+  cfg = config.services.prusa-mqtt;
+in
 {
   options = {
-    services.prusa-mqtt.enable = mkEnableOption "prusa-mqtt";
+    services.prusa-mqtt = {
+      enable = mkEnableOption "prusa-mqtt";
+
+      settings = {
+        description = "Configuration for prusa-mqtt";
+        type = format.type;
+      };
+    };
   };
 
   config = mkIf (cfg.enable) {
     systemd.services.prusa-mqtt = {
       path = with pkgs; [ prusa-mqtt ];
-      script = "prusa-mqtt";
+      script = "prusa-mqtt ${format.generate "config.yaml" cfg.settings}";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
       restart = "always";
